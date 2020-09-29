3649f6f2c01d72106f36127656f232633b03d107
Evelina Berg <evelina.berg@outlook.com>
2020-09-29 16:09:10 +0200

    JS so far

--- script.js
+++ script.js
@@ -8,18 +8,18 @@ const cityID = 276; // Dallas, TX
 const cuisineID = 83; // Seafood
 
 const restaurantAPI = `https://developers.zomato.com/api/v2.1/search?entity_id=${cityID}&entity_type=city&cuisines=${cuisineID}`;
+const restaurantArticle = document.querySelectorAll('.article-main-box');
 
 
 fetch(restaurantAPI, API_KEY)
     .then((response) => {
         return response.json();
     })
-    .then((data) => {
-        data.restaurants.forEach((data) => {
-            console.log(data.restaurant);
-            const getRestaurantInfo = generateRestaurantInfo(data);
+    .then((restaurantResponse) => {
+            const getRestaurantInfo = generateRestaurantInfo(restaurantResponse.restaurants);
             console.log(getRestaurantInfo);
-        })
+            restaurantArticle.innerHTML += getRestaurantHTML(getRestaurantInfo);
+            console.log(getRestaurantInfo);
     });
 
 class RestaurantTemplate {
@@ -32,17 +32,29 @@ class RestaurantTemplate {
     };
 };
 
-const generateRestaurantInfo = data => {
-    const restaurantInfo = new RestaurantTemplate(
-        data.restaurant.thumb,
-        data.restaurant.name,
-        data.restaurant.location.adress,
-        data.restaurant.user_rating.aggregate_rating,
-        data.restaurant.average_cost_for_two,
-    );
+const generateRestaurantInfo = restaurants => {
+    const filteredRestaurants = restaurants.filter(item => item.restaurant.photo_count > 20);
+    const restaurantInfo = filteredRestaurants.map(item => {
+        return new RestaurantTemplate(
+            item.restaurant.thumb,
+            item.restaurant.name,
+            item.restaurant.location.address,
+            item.restaurant.user_rating.aggregate_rating,
+            item.restaurant.average_cost_for_two,
+        );
+    });
     return restaurantInfo;
 };
 
+const getRestaurantHTML = restaurants => {
+    restaurants.forEach(item => {
+        restaurantArticle[index].querySelector('.restaurant-image').src = item.restaurantImage;
+        restaurantArticle[index].querySelector('.restaurant-name').innerText = item.restaurantName;
+        restaurantArticle[index].querySelector('.restaurant-address').innerText = item.restaurantAddress;
+        restaurantArticle[index].querySelector('.restaurant-rating').innerText = ` ${item.restaurantRating}`;
+        restaurantArticle[index].querySelector('.restaurant-price') = item.restaurantAverageCost;
+    });
+}
 
             // document.querySelector('.main-box').innerHTML += `
             // <div class="restaurant">
